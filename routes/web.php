b685709deeaa3df94f2a18d2a6a5640914acbf41
Haidarly2 <haidar.aly007@gmail.com>
2025-05-28 08:45:14 +0700

    Remove duplicate import of DashboardController in routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
