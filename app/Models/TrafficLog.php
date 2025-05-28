5156c5064a2a4a87df049f3e461c3df9b41636f6
Haidarly2 <haidar.aly007@gmail.com>
2025-05-28 08:45:18 +0700

    Add casts for 'recorded_at' attribute in TrafficLog model

--- app/Models/TrafficLog.php
+++ app/Models/TrafficLog.php
@@ -7,4 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class TrafficLog extends Model
 {
     protected $fillable = ['lane', 'vehicle_count', 'recorded_at'];
+    protected $casts = [
+        'recorded_at' => 'datetime',
+    ];
 }
